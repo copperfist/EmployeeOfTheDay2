678bda34be0ad967da930949ed36fedca39ad617
DanielHarrisStudent <56642329+DanielHarrisStudent@users.noreply.github.com>
2019-11-06 00:32:13 +0000

    Hierarchy cleaned. Shopping item by tag
    Cleaned up the hierarchy and added some empty objects to hold everything. Coded in a basic random tag picker

---
+++ EmployeeOfTheDay2/Assets/Scripts/PaperBag.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperBag : MonoBehaviour
+{
+
+    public GameObject shopper;
+    public GameObject entrance;
+
+    public string[] products = new string[6]; //Holds all product tags
+
+    private int randomIndex;
+    private string chosenItem;
+
+    public GameObject item;
+
+
+    private void Start()
+    {        
+        Instantiate(shopper, entrance.transform.position, entrance.transform.rotation);
+
+        FindProducts();
+    }
+    private void FindProducts()
+    {
+        randomIndex = Random.Range(0, products.Length);
+        chosenItem = products[randomIndex];
+        
+        Debug.Log(chosenItem); //The chosen tag
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == chosenItem)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
