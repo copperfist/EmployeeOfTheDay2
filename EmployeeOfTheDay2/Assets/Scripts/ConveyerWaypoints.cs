04bdd0cba94c8cc491aef4e32b742fe11211e425
Ellie Harrodine <ellieharrodine@gmail.com>
2019-11-08 21:38:04 +0000

    waypoints
    still struggling, but I know what the issue is now, just not how to fix it! hoping to ask someone tomorrow

--- EmployeeOfTheDay2/Assets/Scripts/ConveyerWaypoints.cs
+++ EmployeeOfTheDay2/Assets/Scripts/ConveyerWaypoints.cs
@@ -14,18 +14,42 @@ public class ConveyerWaypoints : MonoBehaviour
 
     void OnCollisionStay(Collision collision)
     {
+
+
         //Check if object meets conveyor belt
         if (collision.gameObject.name == "ConveyorMesh")
         {
 
             //Debug.Log("collision with conveyor belt");
-            if(transform.position != target[current].position)
+            if (transform.position != target[current].position)
             {
                 Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
                 GetComponent<Rigidbody>().MovePosition(pos);
+
+                float dist = Vector3.Distance(target[current].position, transform.position);
+
+                //Debug.Log(transform.position.x.ToString());
+                if (dist < 0.1) 
+                {
+                    current++;
+                    Debug.Log("next");
+                    Debug.Log(current);
+                }
+
+                if(current == 3)
+                {
+                    Destroy(gameObject);
+                }
+
             }
-            else current = (current + 1) % target.Length;
         }
+
+        //if (dist <)
+        //{
+        //    Debug.Log("die");
+        //    Destroy(gameObject);
+        //}
+        
 
 
     
