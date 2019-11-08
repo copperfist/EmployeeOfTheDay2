2b3474488f3eee917b9236e35c6b99d4cf598277
Ellie Harrodine <ellieharrodine@gmail.com>
2019-11-08 16:59:25 +0000

    box moves on conveyor belt
    ok the test object moves sort of ok, now it's just about working on the prefabs :(

---
+++ EmployeeOfTheDay2/Assets/Scripts/ConveyerWaypoints.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerWaypoints : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    public float speed;
+    float WPradius = 1;
+    //public GameObject trigger; 
+
+    void Update()
+    {
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)  
+        {
+            
+             current++;
+             if (current >= waypoints.Length)
+                {
+                    Destroy(gameObject);
+            }
+            
+          
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
