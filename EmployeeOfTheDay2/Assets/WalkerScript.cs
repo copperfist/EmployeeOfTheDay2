ac3fece0979e1c31fcde29343ebcc53f5535fa14
DouglasKellett <dug.kellett@gmail.com>
2019-12-01 17:06:24 +0000

    Walking NPC's in menu

---
+++ EmployeeOfTheDay2/Assets/WalkerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkerScript : MonoBehaviour
+{
+
+    public GameObject[] walkerPrefab;
+    private GameObject currentWalker;
+
+
+    void Start()
+    {
+        int walkerIndex = Random.Range(0, walkerPrefab.Length);
+
+        currentWalker = Instantiate(walkerPrefab[walkerIndex], transform.position, transform.rotation);
+
+        currentWalker.transform.parent = this.transform;
+
+    }
+
+
+}
