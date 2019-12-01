f8ad782b43450ed3c2f1324d02b6502708837f9f
Ellie Harrodine <ellieharrodine@gmail.com>
2019-12-01 13:37:32 +0000

    cars move just need to destroy now

---
+++ EmployeeOfTheDay2/Assets/Scripts/carRightSide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carRightSide : MonoBehaviour
+{
+    private float speed = 12f;
+   
+
+
+    private void FixedUpdate()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+
+
+    }
+
+    void OnCollisionEnter()
+    {
+        
+
+    }
+
+}
