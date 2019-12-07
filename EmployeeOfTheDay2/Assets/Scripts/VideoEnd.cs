f85b295ac3284c5b20519983e97694abed137160
Ellie Harrodine <ellieharrodine@gmail.com>
2019-12-07 16:48:25 +0000

    narrative added

---
+++ EmployeeOfTheDay2/Assets/Scripts/VideoEnd.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoEnd : MonoBehaviour
+{
+   public void videoFinished()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
